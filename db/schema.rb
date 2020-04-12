fc590e1a689ce4396213364112760723782f847e
Oleg <gricelevich.oleg@gmail.com>
2020-04-12 23:40:09 +0300

    add column type to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_203129) do
+ActiveRecord::Schema.define(version: 2020_04_12_191422) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
 end
