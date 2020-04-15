8f141c464b4e611b8a7a1fa60621d9b46cb22137
Oleg <gricelevich.oleg@gmail.com>
2020-04-15 17:05:47 +0300

    create user views, add ability to change type

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :username, presence: true
+	validates :username, uniqueness: true
+	validates :type, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
+  resources :users
+  resources :friends, :controller => "users", :type => "Friend"
+  resources :enemies, :controller => "users", :type => "Enemy"
+  resources :neutralities, :controller => "users", :type => "Neutrality"
 end
