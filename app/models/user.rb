6f8750686b3ca009bbb7527d64afcb9c0d5aeb13
Oleg <gricelevich.oleg@gmail.com>
2020-04-22 19:09:04 +0300

    devise + google omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         
-    devise :omniauthable, omniauth_providers: [:google_oauth2]
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+	devise :database_authenticatable, :registerable,
+		:recoverable, :rememberable, :validatable,
+		:omniauthable, omniauth_providers: [:google_oauth2]
+
 	validates :username, presence: true
 	validates :username, uniqueness: true
 	validates :type, presence: true
+
+	def self.create_from_provider_data(provider_data)
+		where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
+			user.email = provider_data.info.email
+			user.username = provider_data.info.email
+			user.type = "Neutrality"
+			user.password = Devise.friendly_token[0, 20]
+		end
+	end
 end
